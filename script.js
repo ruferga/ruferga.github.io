298b11011eeb058953faba3dfe8fb4d42eca4da4
ruferga <lamanoinvisible@gmail.com>
2017-07-11 14:39:20 -0500

    Archivos faltantes

---
+++ script.js
@@ -0,0 +1,40 @@
+$("a.navbar-brand").css("color", "white");
+$("ul.navbar-nav li.page-scroll a.nav-link").css("color", "white");
+$("ul.navbar-nav li.page-scroll.active a.nav-link").css('color', 'white');
+$("a.navbar-brand").css("size", "30px");
+
+
+//Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+ 
+})(jQuery); // End of use strict
